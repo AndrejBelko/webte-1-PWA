34df5334346fc0bc40925c5ef5c160406de6dab9
Lenovo <marianknazo@gmail.com>
2022-12-27 19:45:44 +0100

    funguje info button, zobrazi jeden level

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-var abeceda = document.getElementById("abeceda")
+var abeceda = document.getElementById("abeceda");
+var difficulty = document.getElementById("difficulty");
+var hint = document.getElementById("hint");
+var wordPlace = document.getElementById("wordPlace");
+var hintDiv = document.getElementById("hintDiv")
 var dragged;
 var actDrag = false;
+
+var medium = [];
+var hard = [];
+
+var current;
+
+fetch("./levels.json")
+    .then(res => res.json())
+    .then(data => {
+
+        data.medium.forEach(level => {
+            medium.push(level);
+        })
+
+        data.hard.forEach(level => {
+            hard.push(level);
+        })
+
+        load();
+        getRandomLevel();
+        displayLevel();
+    })
 
 function load(){
     for(var i = 65; i < 91; i++){
@@ -24,6 +50,9 @@ function load(){
         charDiv.addEventListener("dragend",() => {
             if(actDrag){
                 actDrag = false;
+
+                //nejako ze ak to draggol do end zony, nak zmizne, ak inde tak nak ostane, akoze to pismeno aktiualne tahane ved ty vies, ty vies
+                //potom zistic co draggol do end zony a ci sa to nechadza v slove, ak hej span zmenit na pismeno :)
                 charDiv.style.visibility = "visible";
 
             }
@@ -31,6 +60,8 @@ function load(){
         })
         abeceda.appendChild(charDiv);
     }
+    console.log(medium);
+    console.log(hard);
 }
 
 function allowDrop(ev) {
@@ -39,3 +70,51 @@ function allowDrop(ev) {
 }
 
 
+
+function getRandomLevel(){
+
+    // toto random mozno nejako inak zistit lebo v zadani je nejaky divny iny random chcu...
+    let random = Math.floor(Math.random() * 5);
+    console.log(random);
+
+    if (difficulty.innerHTML === "Medium"){
+        for (let i = 0; i < medium.length;i++){
+            if (i === random){
+                current = medium[i];
+            }
+        }
+    } else{
+        for (let i = 0; i < hard.length;i++){
+            if (i === random){
+                current = hard[i];
+            }
+        }
+    }
+}
+
+function displayLevel(){
+    console.log(current);
+    for (let i = 0; i < current.word.length;i++){
+        let a = document.createElement("span");
+        a.innerHTML = " _ ";
+        wordPlace.appendChild(a);
+    }
+}
+
+
+difficulty.addEventListener("click", () => {
+    if (difficulty.innerHTML === "Medium"){
+        difficulty.innerHTML = "Hard";
+    }
+    else{
+        difficulty.innerHTML = "Medium";
+    }
+})
+
+hint.onmouseover = function (){
+    hintDiv.innerHTML = current.clue;
+}
+
+hint.onmouseout = function (){
+    hintDiv.innerHTML = "";
+}
